1b34d54e8ab0f1936f26b40ffe0185c3f5fe2084
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-08-21 12:10:34 +0200

    fix: e2e tests

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://localhost:8000/notebook-elm/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,7 +72,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http:localhost:8000',
+    url: 'http:localhost:8000/notebook-elm/',
     reuseExistingServer: !process.env.CI,
   },
 });
